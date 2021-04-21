0ca427411ad3760f1c622ee7de22fae7ac57bd2f
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2021-04-21 15:38:24 +0200

    Logging, program run cocona and rename

--- src/Apps/DiscordClash.Bot/Program.cs
+++ src/Apps/DiscordClash.Bot/Program.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using Cocona;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using DiscordClash.Bot.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using DiscordClash.Bot.Services;
 
 namespace DiscordClash.Bot
 {
     class Program
     {
-        static void Main(string[] args)
+        private readonly ILogger<Program> _logger;
+
+        public Program(ILogger<Program> logger)
         {
-            Console.WriteLine("Hello World!");
+            _logger = logger;
         }
+
+        static async Task Main(string[] args) =>
+            await CoconaApp.Create()
+                .UseLogger("DiscordClash.Bot")
+                .ConfigureServices((ctx, services) =>
+                {
+                    var configuration = ctx.Configuration;
+                    services.AddSingleton(new DiscordSocketClient(new DiscordSocketConfig
+                    {
+                        LogLevel = LogSeverity.Verbose,
+                        MessageCacheSize = 1000,
+                        AlwaysDownloadUsers = true
+                    }))
+                    .AddSingleton(new CommandService(new CommandServiceConfig
+                    {
+                        LogLevel = LogSeverity.Verbose,
+                        DefaultRunMode = RunMode.Async,
+                    }))
+                    .AddSingleton<CommandHandler>()
+                    //.AddSingleton<StartupService>()
+                    //.AddSingleton<LoggingService>()
+                    ;
+                })
+                .ConfigureAppConfiguration(builder =>
+                {
+                    builder.AddJsonFile("appsettings.json", true, true)
+                        .AddEnvironmentVariables();
+                })
+                .RunAsync<Program>(args);
     }
 }
---
+++ src/Apps/DiscordClash.Bot/Services/CommandHandler.cs
@@ -0,0 +1,46 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+
+namespace DiscordClash.Bot.Services
+{
+    public class CommandHandler
+    {
+        private readonly DiscordSocketClient _discord;
+        private readonly CommandService _commands;
+        private readonly IConfigurationRoot _config;
+        private readonly IServiceProvider _provider;
+
+        public CommandHandler(DiscordSocketClient discord, CommandService commands, IConfigurationRoot config,
+            IServiceProvider provider)
+        {
+            _discord = discord;
+            _commands = commands;
+            _config = config;
+            _provider = provider;
+
+            _discord.MessageReceived += OnMessageReceivedAsync;
+        }
+
+        private async Task OnMessageReceivedAsync(SocketMessage s)
+        {
+            // Ensure the message is from a user/bot
+            if (s is not SocketUserMessage msg) return;
+            if (msg.Author.Id == _discord.CurrentUser.Id) return; // Ignore self when checking commands
+
+            var context = new SocketCommandContext(_discord, msg); // Create the command context
+
+            var argPos = 0; // Check if the message has a valid command prefix
+            if (msg.HasStringPrefix(_config["prefix"], ref argPos) ||
+                msg.HasMentionPrefix(_discord.CurrentUser, ref argPos))
+            {
+                var result = await _commands.ExecuteAsync(context, argPos, _provider); // Execute the command
+
+                if (!result.IsSuccess) // If not successful, reply with the error.
+                    await context.Channel.SendMessageAsync(result.ToString());
+            }
+        }
+    }
+}
