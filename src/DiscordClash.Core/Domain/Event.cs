a314ebc75fd3806b253c940d832b7f54d0fdd6d5
Bartosz Jurczewski <24575781+jurczewski@users.noreply.github.com>
2021-04-23 14:09:35 +0200

    Event, Game, User domain classes (#3) AB#25

---
+++ src/DiscordClash.Core/Domain/Event.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DiscordClash.Core.Domain
+{
+    public class Event
+    {
+        public Guid Id { get; protected set; }
+        public string FullName { get; protected set; }
+        public DateTime StarTime { get; protected set; }
+        public DateTime EndTime { get; protected set; }
+        public string Country { get; protected set; }
+        public string City { get; protected set; }
+        public bool IsFree { get; protected set; }
+        public Game GameCode { get; protected set; }
+        public DateTime CreatedAt { get; protected set; }
+        public DateTime? UpdatedAt { get; protected set; }
+
+        public Event(Guid id, string fullName, DateTime starTime, DateTime endTime, string country, string city, bool isFree, Game gameCode)
+        {
+            Id = id;
+            SetFullName(fullName);
+            SetPeriodTimes(starTime, endTime);
+            Country = country;
+            City = city;
+            IsFree = isFree;
+            GameCode = gameCode;
+
+            CreatedAt = DateTime.Now;
+            UpdatedAt = null;
+        }
+
+        public void SetFullName(string fullName)
+        {
+            if (string.IsNullOrEmpty(fullName))
+            {
+                throw new ValidationException("Event's full name cannot be empty.");
+            }
+
+            FullName = fullName;
+            UpdatedAt = DateTime.Now;
+        }
+
+        public void SetPeriodTimes(DateTime startTime, DateTime endTime)
+        {
+            if (startTime > endTime)
+            {
+                throw new ValidationException("Event's start date cannot be greater than end date.");
+            }
+
+            StarTime = startTime;
+            EndTime = endTime;
+            UpdatedAt = DateTime.Now;
+        }
+    }
+}
---
+++ src/DiscordClash.Core/Domain/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DiscordClash.Core.Domain
+{
+    public class User
+    {
+        public Guid Id { get; protected set; }
+        public string DiscordId { get; protected set; }
+        public string DiscordNickname { get; protected set; }
+        public DateTime CreatedAt { get; protected set; }
+        public DateTime? UpdatedAt { get; protected set; }
+
+        public User(Guid id, string discordId, string discordNickname)
+        {
+            Id = id;
+            SetDiscordData(discordId, discordNickname);
+
+            CreatedAt = DateTime.Now;
+            UpdatedAt = null;
+        }
+
+        public void SetDiscordData(string discordId, string discordNickname)
+        {
+            if (string.IsNullOrEmpty(discordId))
+            {
+                throw new ValidationException("User's discord Id cannot be empty.");
+            }
+
+            if (string.IsNullOrEmpty(discordNickname))
+            {
+                throw new ValidationException("User's discord Id cannot be empty.");
+            }
+
+            DiscordId = discordId;
+            DiscordNickname = discordNickname;
+            UpdatedAt = DateTime.Now;
+        }
+    }
+}
